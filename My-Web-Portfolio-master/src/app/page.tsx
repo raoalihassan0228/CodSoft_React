27bbf1f5ccd86d4b1845b7af3fe5cab7c34ee189
Muhammad Hassan <raoalihassan1145@gmail.com>
2024-01-17 12:59:41 +0500

    Initial commit

---
+++ My-Web-Portfolio-master/src/app/page.tsx
@@ -0,0 +1,18 @@
+import Image from 'next/image'
+import Hero from './component/hero'
+import Contact from './component/contact'
+import Project from './component/project'
+import About from './component/about'
+import Skill from './component/skill'
+
+export default function Home() {
+  return (
+    <div>
+      <Hero />
+      <About />
+      <Skill />
+      <Project />
+      <Contact />
+    </div>
+  )
+}
